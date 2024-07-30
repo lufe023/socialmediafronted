8d0b557e7707aaadeb558cda34cff772c15ba187
Luis Gómez <lufe023@gmail.com>
2024-07-30 16:22:11 -0400

    modificando el index

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        outDir: "dist",
-        rollupOptions: {
-            input: {
-                main: "index.html",
-            },
-        },
-    },
-    publicDir: "public", // Asegúrate de que esto esté configurado correctamente
+    plugins: [react()],
 });
