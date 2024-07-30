ea829ef4b6161250c6f075bca941220592d2820d
Luis Gómez <lufe023@gmail.com>
2024-07-30 16:10:51 -0400

    m

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
     build: {
-        outDir: "dist", // Asegúrate de que esto sea 'dist' y no 'dist/dist'
+        outDir: "dist",
         rollupOptions: {
             input: {
                 main: "index.html",
             },
         },
     },
+    publicDir: "public", // Asegúrate de que esto esté configurado correctamente
 });
