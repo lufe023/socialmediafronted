5ac39e9179efd75e6a97ab66370cb1ad41fa044a
Luis Gómez <lufe023@gmail.com>
2024-07-30 15:58:24 -0400

    Probando nuevas rutas css

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        outDir: "dist", // Asegúrate de que esto sea 'dist' y no 'dist/dist'
+        rollupOptions: {
+            input: {
+                main: "index.html",
+            },
+        },
+    },
+});
