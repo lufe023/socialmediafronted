37f14c53918a487d7b94cea3dbdbf409be243183
Luis Gómez <lufe023@gmail.com>
2024-08-14 16:43:50 -0400

    Corrgiendo carga al desavilitar

--- src/components/AppLayout/user/userDisable.js
+++ src/components/AppLayout/user/userDisable.js
@@ -4,43 +4,36 @@ import getConfig from "../../utils/getConfig";
 
 const userDisable = (user, active) => {
     const URL = `${import.meta.env.VITE_API_SERVER}/api/v1/users/${user}`;
-    axios
-        .patch(URL, { active: active }, getConfig())
-        .then((res) => {
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.addEventListener("mouseenter", Swal.stopTimer);
-                    toast.addEventListener("mouseleave", Swal.resumeTimer);
-                },
-            });
 
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+    });
+
+    axios
+        .patch(URL, { active }, getConfig())
+        .then((res) => {
             Toast.fire({
                 icon: "success",
                 title: "Cambio Exitoso",
             });
         })
         .catch((err) => {
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.addEventListener("mouseenter", Swal.stopTimer);
-                    toast.addEventListener("mouseleave", Swal.resumeTimer);
-                },
-            });
+            const errorMessage =
+                err.response?.data?.message || "Error inesperado";
             console.log(err);
             Toast.fire({
                 icon: "error",
-                title: err.response.data.message,
+                title: errorMessage,
             });
         });
 };
+
 export default userDisable;
